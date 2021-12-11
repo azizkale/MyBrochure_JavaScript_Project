65f9d064e12e565818b894661e4a9ceac079646a
azizkale <azizkale@hotmail.com>
2021-12-11 20:17:48 +0100

    color of textes in div problem was solved

--- js/mainScripts.js
+++ js/mainScripts.js
@@ -244,132 +244,6 @@ function deleteSelectedText() {
   });
 }
 
-//Dragable and Resizable Image
-{
-  //   var element = document.getElementById("blah");
-  //   var x = 0;
-  //   var y = 0;
-  //   interact(element)
-  //     .resizable({
-  //       // resize from all edges and corners
-  //       edges: { left: true, right: true, bottom: true, top: true },
-  //       listeners: {
-  //         move(event) {
-  //           var target = event.target;
-  //           var x = parseFloat(target.getAttribute("data-x")) || 0;
-  //           var y = parseFloat(target.getAttribute("data-y")) || 0;
-  //           // update the element's style
-  //           target.style.width = event.rect.width + "px";
-  //           target.style.height = event.rect.height + "px";
-  //           // translate when resizing from top or left edges
-  //           x += event.deltaRect.left;
-  //           y += event.deltaRect.top;
-  //           target.style.webkitTransform = target.style.transform =
-  //             "translate(" + x + "px," + y + "px)";
-  //           target.setAttribute("data-x", x);
-  //           target.setAttribute("data-y", y);
-  //           target.textContent =
-  //             Math.round(event.rect.width) +
-  //             "\u00D7" +
-  //             Math.round(event.rect.height);
-  //         },
-  //       },
-  //       modifiers: [
-  //         // keep the edges inside the parent
-  //         interact.modifiers.restrictEdges({
-  //           outer: "parent",
-  //         }),
-  //         // minimum size
-  //         interact.modifiers.restrictSize({
-  //           min: { width: 100, height: 50 },
-  //         }),
-  //       ],
-  //       inertia: true,
-  //     })
-  //     .draggable({
-  //       // enable inertial throwing
-  //       inertia: true,
-  //       // keep the element within the area of it's parent
-  //       modifiers: [
-  //         interact.modifiers.restrictRect({
-  //           restriction: "parent",
-  //           endOnly: true,
-  //         }),
-  //       ],
-  //       // enable autoScroll
-  //       autoScroll: true,
-  //       listeners: {
-  //         // call this function on every dragmove event
-  //         move: dragMoveListener,
-  //         // call this function on every dragend event
-  //         end(event) {
-  //           var textEl = event.target.querySelector("p");
-  //           textEl &&
-  //             (textEl.textContent =
-  //               "moved a distance of " +
-  //               Math.sqrt(
-  //                 (Math.pow(event.pageX - event.x0, 2) +
-  //                   Math.pow(event.pageY - event.y0, 2)) |
-  //                   0
-  //               ).toFixed(2) +
-  //               "px");
-  //         },
-  //       },
-  //     });
-  //   function dragMoveListener(event) {
-  //     var target = event.target;
-  //     // keep the dragged position in the data-x/data-y attributes
-  //     var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
-  //     var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
-  //     // translate the element
-  //     target.style.webkitTransform = target.style.transform =
-  //       "translate(" + x + "px, " + y + "px)";
-  //     // update the posiion attributes
-  //     target.setAttribute("data-x", x);
-  //     target.setAttribute("data-y", y);
-  //   }
-  //   // this function is used later in the resizing and gesture demos
-  //   window.dragMoveListener = dragMoveListener;
-}
-
-//Resiazble Preview Area
-{
-  //   var previewArea = document.getElementById("idCerceve");
-  //   interact(previewArea).resizable({
-  //     // resize from all edges and corners
-  //     edges: { left: true, right: true, bottom: true, top: true },
-  //     listeners: {
-  //       move(event) {
-  //         var target = event.target;
-  //         var x = parseFloat(target.getAttribute("data-x")) || 0;
-  //         var y = parseFloat(target.getAttribute("data-y")) || 0;
-  //         // update the element's style
-  //         target.style.width = event.rect.width + "px";
-  //         target.style.height = event.rect.height + "px";
-  //         // translate when resizing from top or left edges
-  //         x += event.deltaRect.left;
-  //         y += event.deltaRect.top;
-  //         target.style.webkitTransform = target.style.transform =
-  //           "translate(" + x + "px," + y + "px)";
-  //         target.setAttribute("data-x", x);
-  //         target.setAttribute("data-y", y);
-  //         //target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
-  //       },
-  //     },
-  //     modifiers: [
-  //       // keep the edges inside the parent
-  //       interact.modifiers.restrictEdges({
-  //         outer: "parent",
-  //       }),
-  //       // minimum size
-  //       interact.modifiers.restrictSize({
-  //         min: { width: 100, height: 50 },
-  //       }),
-  //     ],
-  //     inertia: true,
-  //   });
-}
-
 //Preview Function
 function clickPreview() {
   clickOnizlemeyiTemizle();
@@ -392,6 +266,7 @@ function clickPreview() {
     div.style.right = item.style.right;
     div.style.left = item.style.left;
     div.style.fontSize = item.style.fontSize;
+    div.style.color = item.style.color;
     div.style.position = "absolute";
     item.style.display = "none";
     arrayDivs.push(div);
